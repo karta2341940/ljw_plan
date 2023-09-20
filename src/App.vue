651723936f5b1cafb7498ce03dcb4f30caee0605
MX,Du <38270318+karta2341940@users.noreply.github.com>
2023-09-20 16:54:59 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup>
-import { RouterView } from 'vue-router';
+import { RouterView, RouterLink } from 'vue-router';
 </script>
 
 <template>
     <v-app>
         <v-main>
-            <v-app-bar>
-                <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-                <v-app-bar-title>屏東食品產業數位轉型平台</v-app-bar-title>
-            </v-app-bar>
+
+            <v-toolbar>
+
+
+                <v-toolbar-title>屏東食品產業數位轉型平台</v-toolbar-title>
+
+                <v-spacer></v-spacer>
+                <v-btn>
+                    <RouterLink to="/">Go Home</RouterLink>
+                </v-btn>
+            </v-toolbar>
+
             <RouterView></RouterView>
         </v-main>
     </v-app>
